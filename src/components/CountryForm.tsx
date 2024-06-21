e2fd73fc19597dc6155284e6c8c89de5cd220c01
bibikov nikita <bibikovnikita777@gmail.com>
2024-06-21 14:45:48 +0600

    Add CountryForm component for selecting a country

--- src/components/CountryForm.tsx
+++ src/components/CountryForm.tsx
@@ -1,8 +1,26 @@
+import React from 'react';
 
-const CountryForm = () => {
+interface CountryFormProps {
+    countries: { name: string; alpha3Code: string }[];
+    onSelect: (code: string) => void;
+}
+
+const CountryForm: React.FC<CountryFormProps> = ({ countries, onSelect }) => {
+    const handleChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+        onSelect(event.target.value);
+    };
+
     return (
-        <div>
-
+        <div className="country-form">
+            <h2>Выберите страну</h2>
+            <select onChange={handleChange} defaultValue="">
+                <option value="" disabled>Выберите страну</option>
+                {countries.map(country => (
+                    <option key={country.alpha3Code} value={country.alpha3Code}>
+                        {country.name}
+                    </option>
+                ))}
+            </select>
         </div>
     );
 };
--- src/components/CountryInfo.tsx
+++ src/components/CountryInfo.tsx
@@ -1,5 +1,4 @@
 
-
 const CountryInfo = () => {
     return (
         <div>
